f819daf1678debd82a55d4228928bae709883b11
offici5l <127665988+offici5l@users.noreply.github.com>
2026-01-09 01:38:39 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -34,11 +34,7 @@ app.add_middleware(
     allow_methods=["*"],
 )
 
-SUPPORTED_TARGETS = {
-    "boot.img", "init_boot.img", "dtbo.img", "super_empty.img",
-    "vbmeta.img", "vendor_boot.img", "vendor_kernel_boot.img",
-    "preloader.img", "recovery.img", "logo.img"
-}
+SUPPORTED_TARGETS = {"boot.img","init_boot.img","dtbo.img","super_empty.img","vbmeta.img","vendor_boot.img","vendor_kernel_boot.img","preloader.img","recovery.img","logo.img","abl.img","hyp.img","modem.img","tz.img","xbl.img","lk.img","tee.img","md1img.img","preloader_emmc.img","preloader_raw.img","preloader_ufs.img","vbmeta_system.img","vbmeta_vendor.img","system_dlkm.img","vendor_dlkm.img","aop.img","aop_config.img","bluetooth.img","cpucp.img","cpucp_dtb.img","devcfg.img","dsp.img","featenabler.img","imagefv.img","keymaster.img","qupfw.img","shrm.img","uefi.img","uefisecapp.img","xbl_config.img","xbl_ramdump.img","scp.img","spmfw.img","sspm.img"}
 
 extraction_semaphore = Semaphore(4)
 
